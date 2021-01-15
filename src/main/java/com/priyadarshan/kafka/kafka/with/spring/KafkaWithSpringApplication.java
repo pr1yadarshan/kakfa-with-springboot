9438a39c52cbd5f03ed06048b96bed5eb89d44e0
Priyadarshan <7709011-pr1yadarshan@users.noreply.gitlab.com>
2021-01-15 12:04:08 +0530

    added a message producer

---
+++ src/main/java/com/priyadarshan/kafka/kafka/with/spring/KafkaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.priyadarshan.kafka.kafka.with.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyadarshan/kafka/kafka/with/spring/KafkaWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyadarshan.kafka.kafka.with.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
